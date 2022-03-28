0748315ca7f4ee5d37eeda12eab1cb978b83fd2b
Hai Trong  Do <Hai Do>
2022-03-28 22:07:18 +0700

    cap nhat may tinh nang cao

--- 1. BT Ung Dung May Tinh Nang Cao/May-tinh-nang-cao.js
+++ 1. BT Ung Dung May Tinh Nang Cao/May-tinh-nang-cao.js
@@ -1,5 +1,28 @@
 
+let display = "";
 
-function presskey(key) {
-
+function pressKey(key) {
+    display += key
+    document.getElementById("display-el").innerText = display
+    console.log(display);
 }
+
+
+
+function equal(key) {
+    result = eval(display)
+    document.getElementById("display-el").innerText = result
+}
+
+function clearAll () {
+    display = ""
+    document.getElementById("display-el").innerText = display
+    console.log(display);
+    }
+
+
+function undo() {
+    display = display.slice(display.length - 1 ,1)
+    document.getElementById("display-el").innerText = display
+    }
+
