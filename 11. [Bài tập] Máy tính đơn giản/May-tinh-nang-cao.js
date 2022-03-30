c61f1feb001bc7366d89154b27d0c51625ca66dd
Hai Trong  Do <Hai Do>
2022-03-30 14:41:31 +0700

    cap nhat bai tap may tinh co ban

---
+++ 11. [Bài tập] Máy tính đơn giản/May-tinh-nang-cao.js
@@ -0,0 +1,25 @@
+
+let display = "";
+
+function pressKey(key) {
+    display += key
+    document.getElementById("display-el").innerText = display
+}
+
+
+function equal(key) {
+    result = eval(display)
+    document.getElementById("display-el").innerText = result
+}
+
+function clearAll () {
+    display = ""
+    document.getElementById("display-el").innerText = display
+    }
+
+
+function undo() {
+    display = display.slice(0, -1)
+    document.getElementById("display-el").innerText = display
+    }
+
