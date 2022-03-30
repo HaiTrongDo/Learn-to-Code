8279a750931bbf3ef3979373976a5939550bd4ed
Hai Trong  Do <Hai Do>
2022-03-30 16:30:55 +0700

    cap nhat bai tap do you love me ?

--- 11. [Bài tập] Máy tính đơn giản/May-tinh-nang-cao.js
+++ 11. [Bài tập] Máy tính đơn giản/May-tinh-nang-cao.js
@@ -1,4 +1,3 @@
-
 let display = "";
 
 function pressKey(key) {
@@ -12,14 +11,20 @@ function equal(key) {
     document.getElementById("display-el").innerText = result
 }
 
-function clearAll () {
+function clearAll() {
     display = ""
     document.getElementById("display-el").innerText = display
-    }
+}
 
 
 function undo() {
     display = display.slice(0, -1)
     document.getElementById("display-el").innerText = display
-    }
+}
 
+window.addEventListener("keydown",function (evt) {
+    let c = evt.keyCode;
+    if(c >= 48 && c <= 57)
+        pressKey(evt.key)
+        // document.getElementById("display-el").innerText += evt.key
+})
