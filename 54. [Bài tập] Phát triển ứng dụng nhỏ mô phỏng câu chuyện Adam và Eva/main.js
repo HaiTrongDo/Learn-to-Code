edfd57864a8261760419749054d463a5407965ac
Hai Trong  Do <Hai Do>
2022-04-12 17:23:13 +0700

    Adam va Eva

--- 54. [Bài tập] Phát triển ứng dụng nhỏ mô phỏng câu chuyện Adam và Eva/main.js
+++ 54. [Bài tập] Phát triển ứng dụng nhỏ mô phỏng câu chuyện Adam và Eva/main.js
@@ -27,13 +27,14 @@ class Apple {
 
 
 class Human {
-    constructor(name, gender, weight){
+    constructor(name, gender, weight) {
         this._name = name
         this._gender = gender
         this._weight = weight
     }
-    Human() {
-        if(this._name !=="" && this._gender!=="" && this._weight !==""){
+
+    human() {
+        if (this._name !== "" && this._gender !== "" && this._weight !== "") {
             this._human = true;
         } else {
             this._human = false;
@@ -41,35 +42,53 @@ class Human {
     } //boolean
 
     isMale() {
-        if(this._gender === "Male"){
+        if (this._gender === "Male") {
             return true;
-        } else {return false}
+        } else {
+            return false
+        }
     } //boolean
 
-    setGender(gender){
+    setGender(gender) {
         this._gender = gender;
     }
 
-    checkApple(Apple){
+    checkApple(Apple) {
         return Apple._weight;
     }//boolean
 
-    eat(Apple){
+    eat(Apple) {
         Apple._weight--;
         this._weight++
+        if (Apple._weight <= 0){
+            console.log('táo đã được ăn hết')
+        } else {
+            console.log(`khối lượng táo ${Apple._weight} `)}
     }//
     say(String) {
         alert("say something here")
     } //string
-    setName(String){
+    setName(String) {
         return this._name = String;
     }//String
 
-    getWeight(){
+    getWeight() {
         return this._weight
     }//
-    setWeight(Weight){
+    setWeight(Weight) {
         this._weight = Weight
     }
 
 }
+
+let Adam = new Human("Adam", "Male", 60)
+let Eva = new Human("Eva", "Female", 50)
+let apple = new Apple()
+
+
+for (let i = 0; i < 10; i++) {
+Adam.eat(apple)
+}
+
+console.log(Adam)
+console.log(Eva)
